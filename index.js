73f74375765b9337a5b8eed544ce08aed819f5df
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-02-02 14:28:18 +0100

    update docs

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+/**
+ * @module admin-bro-sequelizejs
+ *
+ * @description
+ * A sequelize database adapter for AdminBro.
+ */
+
+/**
+ * @type {typeof BaseDatabase}
+ * @static
+ */
 const Database = require('./src/database')
+
+/**
+ * @type {typeof BaseResource}
+ * @static
+ */
 const Resource = require('./src/resource')
 
 module.exports = { Database, Resource }
